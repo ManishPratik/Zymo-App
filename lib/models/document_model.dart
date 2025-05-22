b6dfedfdd39070ddb34c512db14af2f047045f0d
Ok-r-on <mohammedomar1604@gmail.com>
2025-05-22 20:53:04 +0530

    FIXED SEARCH BUTTON

---
+++ lib/models/document_model.dart
@@ -0,0 +1,35 @@
+class DocumentModel {
+  late String aadhaarFront;
+  late String aadhaarBack;
+  late String licenseFront;
+  late String licenseBack;
+
+  DocumentModel(
+      {required this.aadhaarFront,
+      required this.aadhaarBack,
+      required this.licenseFront,
+      required this.licenseBack});
+
+  factory DocumentModel.empty() => DocumentModel(
+    aadhaarFront: '',
+    aadhaarBack: '',
+    licenseFront: '',
+    licenseBack: '',
+  );
+
+  factory DocumentModel.fromJson(Map<String, dynamic> json) {
+    return DocumentModel(
+      licenseFront: json['front_page_driving_license']  as String? ?? '',
+      licenseBack:  json['back_page_driving_license']   as String? ?? '',
+      aadhaarFront: json['front_page_aadhaar_card']     as String? ?? '',
+      aadhaarBack:  json['back_page_aadhaar_card']      as String? ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'aadhaarFront':    aadhaarFront,
+    'aadhaarBack':     aadhaarBack,
+    'LicenseFront':    licenseFront,
+    'LicenseBack':     licenseBack,
+  };
+}
