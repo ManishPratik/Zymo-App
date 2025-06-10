32cfe47ff8f4296425e366d2a8ef099260f76c82
RishiMehtaa <merit1607@gmail.com>
2025-06-10 16:45:53 +0530

    added uimodel

---
+++ lib/models/UImodel.dart
@@ -0,0 +1,10 @@
+class StepModel {
+  final String imagePath;
+  final String title;
+  final String description;
+
+  StepModel(
+      {required this.imagePath,
+        required this.title,
+        required this.description});
+}
